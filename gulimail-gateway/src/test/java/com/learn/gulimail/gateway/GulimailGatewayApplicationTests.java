e26f90af9e77018d0a963b4c75279fe30c56537d
chenfuyuan <chenfuyuan0713@163.com>
2020-07-28 05:53:19 +0800

    配置API网关

---
+++ gulimail-gateway/src/test/java/com/learn/gulimail/gateway/GulimailGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.gulimail.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimailGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
