79c9db6aad9954b960895b96082998cfe53172c4
chenfuyuan <chenfuyuan0713@163.com>
2021-12-05 14:23:57 +0800

    feat(仓储服务): 创建项目结构-仓储服务

---
+++ mall-ware/src/main/java/com/learn/project/mall/ware/MallWareApplication.java
@@ -0,0 +1,13 @@
+package com.learn.project.mall.ware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallWareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallWareApplication.class, args);
+    }
+
+}
---
+++ mall-ware/src/test/java/com/learn/project/mall/ware/MallWareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.project.mall.ware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallWareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
