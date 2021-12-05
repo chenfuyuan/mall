976cc1e96cbc073859536422afcf6aad8c28dd8b
chenfuyuan <chenfuyuan0713@163.com>
2021-12-05 14:22:48 +0800

    feat(优惠券服务): 创建项目结构-优惠券服务

---
+++ mall-coupon/src/test/java/com/learn/project/mall/coupon/MallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.project.mall.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
