58274867361720ff6324ee1004720124138a4afc
chenfuyuan <chenfuyuan0713@163.com>
2021-12-06 17:45:16 +0800

    feat(common): 创建公共模块，并引入由代码生成器生成的代码所需要的依赖及类

---
+++ mall-common/src/main/java/com/learn/project/common/utils/Constant.java
@@ -0,0 +1,30 @@
+package com.learn.project.common.utils;
+
+/**
+ * 常量类，用于存放常量
+ *
+ * @author chenfuyuan
+ * @date 2021/12/6 17:25
+ */
+public class Constant {
+    /**
+     * 当前页码
+     */
+    public static final String PAGE = "page";
+    /**
+     * 每页显示记录数
+     */
+    public static final String LIMIT = "limit";
+    /**
+     * 排序字段
+     */
+    public static final String ORDER_FIELD = "sidx";
+    /**
+     * 排序方式
+     */
+    public static final String ORDER = "order";
+    /**
+     *  升序
+     */
+    public static final String ASC = "asc";
+}
