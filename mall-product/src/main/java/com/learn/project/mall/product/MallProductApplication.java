3f75cfef14ed68b9ce8d4e923fde596ba1bdf409
chenfuyuan <chenfuyuan0713@163.com>
2021-12-08 16:52:53 +0800

    feat(product): 整合OpenFeign

--- mall-product/src/main/java/com/learn/project/mall/product/MallProductApplication.java
+++ mall-product/src/main/java/com/learn/project/mall/product/MallProductApplication.java
@@ -4,8 +4,10 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @MapperScan("com.learn.project.mall.product.dao")
+@EnableFeignClients(basePackages = "com.learn.project.mall.product.feign")
 @EnableDiscoveryClient    //开启服务注册与发现
 @SpringBootApplication
 public class MallProductApplication {
