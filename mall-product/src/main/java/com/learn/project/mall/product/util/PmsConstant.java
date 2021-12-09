066e779b87ed1b614221592a5812b3f384a95c0e
chenfuyuan <chenfuyuan0713@163.com>
2021-12-09 15:16:15 +0800

    feat(product): 查询所有分类及其子分类，并将其组装成树型列表。

---
+++ mall-product/src/main/java/com/learn/project/mall/product/util/PmsConstant.java
@@ -0,0 +1,14 @@
+package com.learn.project.mall.product.util;
+
+/**
+ * 商品服务的常量类
+ *
+ * @author chenfuyuan
+ * @date 2021/12/9 14:45
+ */
+public class PmsConstant {
+    /**
+     * 一级菜单
+     */
+    public static final int CATEGORT_LEVEL_ONE = 1;
+}
